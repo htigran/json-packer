2132b06e7a96ba6abfa76f9fc6353c0d861e6a55
Tigran <email@thetigran.com>
2022-11-04 17:18:56 +0400

    syntax fix

--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['box_5fserialize_5fkeys_0',['box_serialize_keys',['../json-parsert_8c.html#a1aa7c2085f9378f16b8e17afcbfa725e',1,'box_serialize_keys(yajl_val node):&#160;json-parsert.c'],['../json-parsert_8h.html#a1aa7c2085f9378f16b8e17afcbfa725e',1,'box_serialize_keys(yajl_val node):&#160;json-parsert.c']]],
-  ['box_5fserialize_5fvalues_1',['box_serialize_values',['../json-parsert_8c.html#a7aa94be473a6fa7069ddfc8d51b3c11e',1,'box_serialize_values(yajl_val node):&#160;json-parsert.c'],['../json-parsert_8h.html#a7aa94be473a6fa7069ddfc8d51b3c11e',1,'box_serialize_values(yajl_val node):&#160;json-parsert.c']]]
+  ['box_5fserialize_5fkeys_0',['box_serialize_keys',['../json-parser_8c.html#a1aa7c2085f9378f16b8e17afcbfa725e',1,'box_serialize_keys(yajl_val node):&#160;json-parser.c'],['../json-parser_8h.html#a1aa7c2085f9378f16b8e17afcbfa725e',1,'box_serialize_keys(yajl_val node):&#160;json-parser.c']]],
+  ['box_5fserialize_5fvalues_1',['box_serialize_values',['../json-parser_8c.html#a7aa94be473a6fa7069ddfc8d51b3c11e',1,'box_serialize_values(yajl_val node):&#160;json-parser.c'],['../json-parser_8h.html#a7aa94be473a6fa7069ddfc8d51b3c11e',1,'box_serialize_values(yajl_val node):&#160;json-parser.c']]]
 ];
